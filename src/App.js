b1bb7f2ae2bbf4c10a51ca79ad3e51031918b741
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2022-12-09 22:18:09 -0300

    committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+
 
 function App() {
+  const [isLoaded,setIsLoaded] = useState(false);
+  const [item,setItem] = useState([]);
+  const [error,setError] = useState(new Error());
+  const [counter,setCounter] = useState(1);
+
+    useEffect(() => {
+      fetch(`https://rickandmortyapi.com/api/character/${counter}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItem(result);
+            console.log(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+            console.log(error);
+          }
+        )
+    },[counter])
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <div>
+            <button onClick={()=>setCounter(counter+1)}>Proximo</button>
+            <h1>{counter}</h1>
+            <button onClick={()=>setCounter(counter-1)}>Anterior</button>
+          </div>
+              {
+                item && 
+                
+                <div>
+                  <h1>{item.name}</h1>
+                  <h1>{item.gender}</h1>
+                  <img src={item.image}/>
+                </div>
+                  
+              }  
+        </div>
     </div>
   );
 }
